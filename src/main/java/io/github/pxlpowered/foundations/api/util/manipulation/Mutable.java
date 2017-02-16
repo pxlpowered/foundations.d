7acbabb15ed2bb4bc6eb544a96c6875462f813dc
kinggoesgaming <hunar.roop@gmail.com>
2017-02-16 12:56:38 -0800

    add Immutable/Mutable interfaces
    Signed-off-by: kinggoesgaming <hunar.roop@gmail.com>

---
+++ src/main/java/io/github/pxlpowered/foundations/api/util/manipulation/Mutable.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of foundations, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2017 - 2017 PxL Powered <https://pxlpowered.github.io/foundations>
+ * Copyright (c) Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.github.pxlpowered.foundations.api.util.manipulation;
+
+/**
+ * Marks an mutable class for the immutable type {@link I}.
+ *
+ * @apiNote A single class should not implement both {@link Mutable} and
+ * {@link Immutable} interfaces, as it undermines the whole purpose of having
+ *     the variants.
+ * @param <I> The immutable type.
+ */
+public interface Mutable<I extends Immutable<? extends Mutable<I>>> {
+
+    /**
+     * Gets a new immutable variant of the class.
+     *
+     * @return The immutable instance.
+     */
+    I asImmutable();
+
+}
